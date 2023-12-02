4004a55e59dc1d140799dc0f65186770c0187685
Daulet Dulanov <dulanov+github@gmail.com>
2023-12-02 10:42:22 +0000

    feat: add day02 solution for part 2
    Signed-off-by: GitHub <noreply@github.com>

--- 2023/day02.py
+++ 2023/day02.py
@@ -12,6 +12,14 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """
 
+example2 = """\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+
 
 class RGB:
     """RGB color representation of sets of cubes."""
@@ -31,12 +39,12 @@ def part_one(
     """Solve part one of the puzzle.
 
     >>> part_one(example1.splitlines())
-    [(1, True), (2, True), (3, False), (4, False), (5, True)]
+    [1, 2, 5]
 
-    >>> sum([t[0] for t in part_one(example1.splitlines()) if t[1]])
+    >>> sum(part_one(example1.splitlines()))
     8
 
-    >>> sum([t[0] for t in part_one(read(f"2023/day{day}.in")) if t[1]])
+    >>> sum(part_one(read(f"2023/day{day}.in")))
     2101
     """
     i, result = 0, []
@@ -46,13 +54,30 @@ def part_one(
             if set.r > limits.r or set.g > limits.g or set.b > limits.b:
                 win = False
                 break
-        result.append((i, win))
+        if win:
+            result.append(i)
     return result
 
 
 def part_two(puzzle: Iterable[str]) -> list[int]:
-    """Solve part two of the puzzle."""
-    return []
+    """Solve part two of the puzzle.
+
+    >>> part_two(example2.splitlines())
+    [48, 12, 1560, 630, 36]
+
+    >>> sum(part_two(example2.splitlines()))
+    2286
+
+    >>> sum(part_two(read(f"2023/day{day}.in")))
+    58269
+    """
+    result = []
+    for game in scan(puzzle):
+        r, g, b = 0, 0, 0
+        for set in game:
+            r, g, b = max(r, set.r), max(g, set.g), max(b, set.b)
+        result.append(r * g * b)
+    return result
 
 
 def scan(puzzle: Iterable[str]) -> Iterator[list[RGB]]:
